b78f9f5704164532e568b54184f9f8f243061677
Kerry Lang <klang812@gmail.com>
2021-04-23 10:21:05 -0700

    add keg detail component

---
+++ src/components/KegDetail.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function KegDetail(props){
+  const { keg, onClickingBuy, onClickingRestock, onClickingDelete } = props;
+
+  return (
+    <React.Fragment>
+      <h1>Keg Detail</h1>
+      <h3>{keg.name}</h3>
+      <p>{keg.brand}</p>
+      <p>${keg.price}</p>
+      <p>{keg.alcoholContect}%</p>
+      <p>Pints Left: {keg.quantity}</p>
+      {keg.quantity > 0 && 
+        <button onclick={() => onClickingBuy(keg.id)}>Sold a pint</button>
+      }
+      {keg.quantity === 0 &&
+        <h1>Sorry, no more beer!</h1>
+      }
+      <button onClick={()=> onClickingRestock(keg.id)}>Restock pints</button>
+      <button onClick={()=> onClickingDelete(keg.id)}>Delete keg</button>
+    </React.Fragment>
+  );
+}
+
+KegDetail.propTypes = {
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingRestock: PropTypes.func,
+  onClickingBuy: PropTypes.func
+};
+
+export default KegDetail;
